5fb75124a920cbbe7f0f0e4c18e43b4513418497
MarloA1987 <marloajeno28@gmail.com>
2016-07-21 09:58:02 +0800

    update sale transactions

--- CAMIS/forms/frmSalesTransaction.Designer.vb
+++ CAMIS/forms/frmSalesTransaction.Designer.vb
@@ -25,18 +25,22 @@ Partial Class frmSalesTransaction
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
         Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.txtSearch = New MetroFramework.Controls.MetroTextBox()
         Me.Panel3 = New System.Windows.Forms.Panel()
-        Me.ListView1 = New System.Windows.Forms.ListView()
         Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.ListView1 = New System.Windows.Forms.ListView()
         Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtSearch = New MetroFramework.Controls.MetroTextBox()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lTotal = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
         Me.Panel3.SuspendLayout()
         Me.Panel4.SuspendLayout()
+        Me.Panel5.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
@@ -47,35 +51,95 @@ Partial Class frmSalesTransaction
         Me.Panel1.Controls.Add(Me.FlowLayoutPanel1)
         Me.Panel1.Location = New System.Drawing.Point(12, 75)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(613, 417)
+        Me.Panel1.Size = New System.Drawing.Size(613, 399)
         Me.Panel1.TabIndex = 0
         '
         'FlowLayoutPanel1
         '
+        Me.FlowLayoutPanel1.BackColor = System.Drawing.Color.Brown
         Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 0)
         Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
-        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(613, 417)
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(613, 399)
         Me.FlowLayoutPanel1.TabIndex = 0
         '
         'Panel2
         '
-        Me.Panel2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel2.Controls.Add(Me.Panel5)
         Me.Panel2.Controls.Add(Me.Panel3)
-        Me.Panel2.Controls.Add(Me.Button1)
         Me.Panel2.Location = New System.Drawing.Point(631, 75)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(501, 417)
+        Me.Panel2.Size = New System.Drawing.Size(501, 410)
         Me.Panel2.TabIndex = 1
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.Panel4)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel3.Location = New System.Drawing.Point(0, 0)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(501, 410)
+        Me.Panel3.TabIndex = 1
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.ListView1)
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel4.Location = New System.Drawing.Point(0, 0)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(501, 410)
+        Me.Panel4.TabIndex = 0
+        '
+        'ListView1
+        '
+        Me.ListView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListView1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.ListView1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.GridLines = True
+        Me.ListView1.Location = New System.Drawing.Point(3, 3)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(498, 301)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "DESCRIPTION"
+        Me.ColumnHeader1.Width = 345
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "QTY"
+        Me.ColumnHeader2.Width = 81
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "PRICE"
+        Me.ColumnHeader3.Width = 71
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(389, 341)
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.BackColor = System.Drawing.Color.Tomato
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Salmon
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSalmon
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Maroon
+        Me.Button1.Location = New System.Drawing.Point(377, 16)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(109, 73)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Text = "Transact"
+        Me.Button1.UseVisualStyleBackColor = False
         '
         'txtSearch
         '
@@ -108,58 +172,46 @@ Partial Class frmSalesTransaction
         Me.txtSearch.WaterMarkColor = System.Drawing.Color.FromArgb(CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer))
         Me.txtSearch.WaterMarkFont = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         '
-        'Panel3
+        'Panel5
         '
-        Me.Panel3.Controls.Add(Me.Panel4)
-        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
-        Me.Panel3.Location = New System.Drawing.Point(0, 0)
-        Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(501, 335)
-        Me.Panel3.TabIndex = 1
+        Me.Panel5.Controls.Add(Me.lTotal)
+        Me.Panel5.Controls.Add(Me.Label1)
+        Me.Panel5.Controls.Add(Me.Button1)
+        Me.Panel5.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel5.Location = New System.Drawing.Point(0, 310)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(501, 100)
+        Me.Panel5.TabIndex = 2
         '
-        'ListView1
+        'Label1
         '
-        Me.ListView1.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
-        Me.ListView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.ListView1.FullRowSelect = True
-        Me.ListView1.GridLines = True
-        Me.ListView1.Location = New System.Drawing.Point(0, 0)
-        Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(501, 304)
-        Me.ListView1.TabIndex = 0
-        Me.ListView1.UseCompatibleStateImageBehavior = False
-        Me.ListView1.View = System.Windows.Forms.View.Details
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label1.Location = New System.Drawing.Point(14, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(130, 21)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "TOTAL AMOUNT"
         '
-        'Panel4
+        'lTotal
         '
-        Me.Panel4.Controls.Add(Me.ListView1)
-        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.Panel4.Location = New System.Drawing.Point(0, 31)
-        Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(501, 304)
-        Me.Panel4.TabIndex = 0
-        '
-        'ColumnHeader1
-        '
-        Me.ColumnHeader1.Text = "DESCRIPTION"
-        Me.ColumnHeader1.Width = 345
-        '
-        'ColumnHeader2
-        '
-        Me.ColumnHeader2.Text = "QTY"
-        Me.ColumnHeader2.Width = 78
-        '
-        'ColumnHeader3
-        '
-        Me.ColumnHeader3.Text = "PRICE"
+        Me.lTotal.AutoSize = True
+        Me.lTotal.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lTotal.Font = New System.Drawing.Font("Verdana", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lTotal.ForeColor = System.Drawing.Color.DarkRed
+        Me.lTotal.Location = New System.Drawing.Point(98, 50)
+        Me.lTotal.Name = "lTotal"
+        Me.lTotal.Size = New System.Drawing.Size(46, 25)
+        Me.lTotal.TabIndex = 3
+        Me.lTotal.Text = "0.0"
         '
         'frmSalesTransaction
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.DarkSalmon
-        Me.ClientSize = New System.Drawing.Size(1144, 504)
+        Me.ClientSize = New System.Drawing.Size(1144, 486)
         Me.Controls.Add(Me.txtSearch)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
@@ -169,6 +221,8 @@ Partial Class frmSalesTransaction
         Me.Panel2.ResumeLayout(False)
         Me.Panel3.ResumeLayout(False)
         Me.Panel4.ResumeLayout(False)
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -185,4 +239,7 @@ Partial Class frmSalesTransaction
     Friend WithEvents ColumnHeader2 As ColumnHeader
     Friend WithEvents ColumnHeader3 As ColumnHeader
     Friend WithEvents btn As Button
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lTotal As Label
 End Class
--- CAMIS/forms/frmSalesTransaction.vb
+++ CAMIS/forms/frmSalesTransaction.vb
@@ -7,8 +7,8 @@
         SqlReFill("saledata", lv)
         For i = 0 To ds.Tables("saledata").Rows.Count - 1
             Dim btn As New Button()
-            btn.Width = 109
-            btn.Height = 73
+            btn.Width = 140
+            btn.Height = 90
             btn.Name = itemcount
             btn.Text = ds.Tables("saledata").Rows(i).Item(0).ToString
             itemcount += 1
@@ -22,10 +22,34 @@
         indexMenu = button.Name
         Dim foodMenu As String = button.Text
         '        MessageBox.Show(ds.Tables("saledata").Rows(indexMenu).Item(2).ToString)
-        Dim lv As New ListViewItem(ds.Tables("saledata").Rows(indexMenu).Item(0).ToString)
-        lv.SubItems.Add(1)
-        lv.SubItems.Add(ds.Tables("saledata").Rows(indexMenu).Item(2).ToString)
-        ListView1.Items.Add(lv)
+        Dim description As String = ds.Tables("saledata").Rows(indexMenu).Item(0).ToString
+        Dim inList As Boolean = False
+        For i = 0 To ListView1.Items.Count - 1
+            If description = ListView1.Items(i).Text Then
+                ListView1.Items(i).SubItems(1).Text += 1
+                inList = True
+                compute()
+                Exit Sub
+            End If
+        Next
+        If inList = False Then
+            Dim lv As New ListViewItem(description)
+            lv.SubItems.Add(1)
+            lv.SubItems.Add(ds.Tables("saledata").Rows(indexMenu).Item(2).ToString)
+            ListView1.Items.Add(lv)
+            inList = False
+            compute()
+        End If
+        'compute sum
+
+
+    End Sub
+    Sub compute()
+        Dim total As Double = 0.0
+        For i = 0 To ListView1.Items.Count - 1
+            total += Double.Parse(ListView1.Items(i).SubItems(1).Text * ListView1.Items(i).SubItems(2).Text)
+        Next
+        lTotal.Text = total.ToString("C", Globalization.CultureInfo.GetCultureInfo("en-PH"))
     End Sub
     Dim itemcount As String = 0
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -38,4 +62,7 @@
         'FlowLayoutPanel1.Controls.Add(btn)
     End Sub
 
+    Private Sub Panel5_Paint(sender As Object, e As PaintEventArgs) Handles Panel5.Paint
+
+    End Sub
 End Class
