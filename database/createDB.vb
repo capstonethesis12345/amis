56df7be6d797a8a268c81eaba26043423874bae0
MarloA1987 <marloajeno28@gmail.com>
2016-07-23 15:24:08 +0800

    Update transaction

--- database/createDB.vb
+++ database/createDB.vb
@@ -52,7 +52,7 @@ Public Class createDB
 & "   `deleted` tinyint(1) Not NULL Default '0', " _
 & "   `EmploymentStarted` date Not NULL, " _
 & "   PRIMARY KEY (`EmpID`) " _
-& " ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ; ")
+& " ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ; ")
         '3
         sqlList.Add("CREATE TABLE IF Not EXISTS `users` (
               `UserID` int(30) NOT NULL AUTO_INCREMENT,
